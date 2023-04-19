3b6826e3c35f19ac77de5c9a3d558871ede0833c
m4c130d <m4c130d@protonmail.com>
2023-04-19 16:12:54 -0400

    netcat modification

--- chapter1/netcat.py
+++ chapter1/netcat.py
@@ -6,24 +6,34 @@ import sys
 import textwrap
 import threading
 
+
+def execute(cmd):
+    cmd = cmd.strip()
+    if not cmd:
+        return
+    output = subprocess.check_output(shlex.split(cmd),
+                                     stderr=subprocess.STDOUT)
+    return output.decode()
+
+
 class NetCat:
     def __init__(self, args, buffer=None):
         self.args = args
         self.buffer = buffer
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        
+
     def run(self):
         if self.args.listen:
             self.listen()
         else:
             self.send()
-            
+
     def send(self):
         self.socket.connect((self.args.target, self.args.port))
         if self.buffer:
             self.socket.send(self.buffer)
-        
+
         try:
             while True:
                 recv_len = 1
@@ -43,43 +53,41 @@ class NetCat:
             print('User terminated.')
             self.socket.close()
             sys.exit()
-            
-            
+
     def listen(self):
+        print('listening')
         self.socket.bind((self.args.target, self.args.port))
         self.socket.listen(5)
-        
         while True:
-            client_socket, _ =self.socket.accept()
-            client_thread = threading.Thread(
-                target=self.handle, args=(client_socket,)
-            )
+            client_socket, _ = self.socket.accept()
+            client_thread = threading.Thread(target=self.handle, args=(client_socket,))
             client_thread.start()
-            
+
     def handle(self, client_socket):
         if self.args.execute:
             output = execute(self.args.execute)
             client_socket.send(output.encode())
-        
+
         elif self.args.upload:
             file_buffer = b''
             while True:
                 data = client_socket.recv(4096)
                 if data:
                     file_buffer += data
+                    print(len(file_buffer))
                 else:
                     break
 
             with open(self.args.upload, 'wb') as f:
                 f.write(file_buffer)
-                message = f'Saved file {self.args.upload}'
-                client_socket.send(message.encode())
-            
+            message = f'Saved file {self.args.upload}'
+            client_socket.send(message.encode())
+
         elif self.args.command:
             cmd_buffer = b''
             while True:
                 try:
-                    client_socket.send(b'BGP: #> ')
+                    client_socket.send(b' #> ')
                     while '\n' not in cmd_buffer.decode():
                         cmd_buffer += client_socket.recv(64)
                     response = execute(cmd_buffer.decode())
@@ -90,35 +98,20 @@ class NetCat:
                     print(f'server killed {e}')
                     self.socket.close()
                     sys.exit()
-                    
-                    
 
 
-def execute(cmd):
-    cmd = cmd.strip()
-    if not cmd:
-        return
-    # shlex split la string en syntaxe shell-like
-    # capture_output permet de capturer l'output sous variable.stdout
-    output = subprocess.run(shlex.split(cmd), capture_output=True)
-    return output
-
-
-
-if __name__=='__main__':
+if __name__ == '__main__':
     parser = argparse.ArgumentParser(
         description='BHP Net Tool',
-        # RawDescriptionHelpFormater garde le formatage de la description et de l'épilogue
         formatter_class=argparse.RawDescriptionHelpFormatter,
-        #textwrap.dedent() enlève l'indentation par défaut des triple quote (multi ligne)
         epilog=textwrap.dedent('''Example:
-            netcat.py -t 192.168.1.108 -p 5555 -l -c # command shell
-            netcat.py -t 192.168.1.108 -u=mytest.txt # upload file
-            netcat.py -t 192.168.1.108 -l 5555 -l -e=\"cat /etc/passwd\" # execute command
-            echo 'ABC' | ./netcat.py -t 192.168.1.108 -p 135 # echo text to server port 135
-            netcat.py -t 192.168.1.108 -p 5555 # connect to server
-            '''))
-    parser.add_argument('-c', '--command', action='store_true', help='command shell')
+          netcat.py -t 192.168.1.108 -p 5555 -l -c # command shell
+          netcat.py -t 192.168.1.108 -p 5555 -l -u=mytest.whatisup # upload to file
+          netcat.py -t 192.168.1.108 -p 5555 -l -e=\"cat /etc/passwd\" # execute command
+          echo 'ABCDEFGHI' | ./netcat.py -t 192.168.1.108 -p 135 # echo local text to server port 135
+          netcat.py -t 192.168.1.108 -p 5555 # connect to server
+          '''))
+    parser.add_argument('-c', '--command', action='store_true', help='initialize command shell')
     parser.add_argument('-e', '--execute', help='execute specified command')
     parser.add_argument('-l', '--listen', action='store_true', help='listen')
     parser.add_argument('-p', '--port', type=int, default=5555, help='specified port')
@@ -129,9 +122,6 @@ if __name__=='__main__':
         buffer = ''
     else:
         buffer = sys.stdin.read()
-        
-    nc = NetCat(args, buffer.encode())
-    nc.run()
-        
 
-            
+    nc = NetCat(args, buffer.encode('utf-8'))
+    nc.run()
